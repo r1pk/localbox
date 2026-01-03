7b5695623d632e5467eaf902bd1d07b6771a5ca0
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-01-03 21:58:54 +0100

    chore: install dropzone package

--- importmap.php
+++ importmap.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Returns the importmap for this application.
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
 return [
     'app' => [
         'path' => './assets/app.js',
@@ -10,5 +18,11 @@ return [
     ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    'dropzone' => [
+        'version' => '6.0.0-beta.2',
+    ],
+    'just-extend' => [
+        'version' => '5.1.1',
     ],
 ];
