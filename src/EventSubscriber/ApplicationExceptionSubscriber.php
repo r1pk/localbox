218b78d5cbab9cd70c0d35e212d83826a9a1f77f
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-06-12 21:38:37 +0200

    fix: make subscriber ignore unknown exceptions

--- src/EventSubscriber/ApplicationExceptionSubscriber.php
+++ src/EventSubscriber/ApplicationExceptionSubscriber.php
@@ -5,7 +5,6 @@ namespace App\EventSubscriber;
 use App\Exception\ApplicationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 class ApplicationExceptionSubscriber implements EventSubscriberInterface
@@ -19,18 +18,14 @@ class ApplicationExceptionSubscriber implements EventSubscriberInterface
 
     public function onException(ExceptionEvent $event): void
     {
-        $payload = ['error' => 'An unexpected server error occurred'];
-        $code = Response::HTTP_INTERNAL_SERVER_ERROR;
-
         $exception = $event->getThrowable();
 
         if ($exception instanceof ApplicationException) {
-            $payload = $exception->getResponsePayload();
-            $code = $exception->getResponseStatus();
+            $event->setResponse(
+                new JsonResponse(
+                    $exception->getResponsePayload(), $exception->getResponseStatus(),
+                ),
+            );
         }
-
-        $event->setResponse(
-            new JsonResponse($payload, $code),
-        );
     }
 }
